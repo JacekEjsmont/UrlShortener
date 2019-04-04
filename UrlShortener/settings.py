f460fa2100b9854ddfe47316b0acd29df6c4bdf1
JacekEjsmont <jacek.ejsmont@gmail.com>
2019-04-05 01:43:17 +0200

    fix for heroku2

--- UrlShortener/settings.py
+++ UrlShortener/settings.py
@@ -77,11 +77,18 @@ WSGI_APPLICATION = 'UrlShortener.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-          default='sqlite:////{0}'.format(os.path.join(BASE_DIR, 'db.sqlite3'))
-      )
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'urlje',
+        'USER': 'name',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
+}
 
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
